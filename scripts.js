3c0d2edfa7f20975f59a29c74607b664db2b26fa
Rethabile Diale <rethadiale27@gmail.com>
2024-05-07 21:46:33 +0200

    Used this code to check if either the dividend or divider is empty.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,11 @@ form.addEventListener("submit", (event) => {
 const dividend = document.querySelector('input[name="dividend"]').value;
 const divider = document.querySelector('input[name="divider"]').value;
 
+if (dividend === "" || divider === "") {
+  result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+  return;
+}
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor(dividend / divider);
