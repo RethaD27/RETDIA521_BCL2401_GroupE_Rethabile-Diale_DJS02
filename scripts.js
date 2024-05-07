674dd46d96286e3cd08602ddd4799e5ac8863cac
Rethabile Diale <rethadiale27@gmail.com>
2024-05-07 21:43:22 +0200

    Added these lines as to extract the values of the dividend and divider inputs from the form.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+const dividend = document.querySelector('input[name="dividend"]').value;
+const divider = document.querySelector('input[name="divider"]').value;
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor(dividend / divider);
